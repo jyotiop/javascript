bc71f9639155a6f9569a742fe8d53836accd25f6
Jyoti kuswaha <kushwahajyoti76881@gmail.com>
2025-10-16 16:00:09 +0000

    datatypes

---
+++ basics/datatypes.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all JS code as newer version
+
+
+// alert(3+3) // we are using nodejs not in browser
+
+console.log(3+3); // code readability should be high
+
+console.log("jyoti");
+
+let name = "jyoti"
+let age = 21
+let isapplyIn = "false"
+let city;
+
+
+
+// number => 2 to power of 53
+//bigint
+//string => ""
+//boolean => true/false
+//null =>standalone value
+//undefined =>
+// symbol=> unique
+
+// object
+
+console.log(typeof undefined); // this output of undefined shows the undefined
+console.log(typeof null); // null datatypes output shows the object
+
