24ec65ad419354820742b36594cdbd7708b4a36d
Jyoti kuswaha <kushwahajyoti76881@gmail.com>
2025-10-16 16:02:03 +0000

    variable

---
+++ basics/variable.js
@@ -0,0 +1,20 @@
+const studenntId = 4727
+let studentName = "shivam"
+let studentEmail = "admin@gmail.com"
+let studentPassword = "1234"
+let studentCourse = "b.tech"
+let studentCity;
+
+studentName = "Harshit"
+studentEmail = "abc@gmail.com"
+studentPassword = "12345"
+studentCourse = "b.tech"
+
+
+/*
+prefer var is not used in javaScript
+because of issue in block scope and functional scope
+*/
+
+console.log(studenntId);
+console.table([studentName, studentEmail, studentPassword, studentCourse, studentCity]);
