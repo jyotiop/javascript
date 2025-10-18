570e4081a2528ad66dd182e100e263416b793f4c
Jyoti kuswaha <kushwahajyoti76881@gmail.com>
2025-10-18 15:58:31 +0000

    conversionoperations

---
+++ basics/conversionoperations.js
@@ -0,0 +1,30 @@
+// console.log(typeof number);
+// console.log(typeof string);
+
+// let valueInNumber = 223
+// console.log(typeof valueInNumber );
+// console.log(valueInNumber)
+
+
+// let Boolean = 0
+// console.log(typeof boolean);
+
+
+// let isLoggedIn = "jyoti"
+
+// let booleanIsLoogedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoogedIn);
+
+// 1 => boolean value converting into the true and 0 => boolean value convertign in to the false.
+// if boolean value is empty = "" is output shows the false
+// if boolean value in the string like"jyoti" it shows the output is true.
+
+
+// ---------------Operations------------------
+
+let num1 = 7
+let num2 = 5
+let num3 = num1 + num2
+
+// console.log(num3)
+console.log( (1+2) +"3")
